6309007cc7433cf4dc4a32cb9d5edf0179ec8b02
Egor <itpd2010@yandex.ru>
2021-08-17 22:41:43 +0300

    feat: test ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }
+  let minChar = array.length ? array[0] : 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minChar) {
+      minChar = array[i];
+    }
+  }
+  return minChar
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }
+  let minChar = array.length ? array[0] : 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > minChar) {
+      minChar = array[i];
+    }
+  }
+  return minChar
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  let avg = array.length ? sum / array.length : 0;
+  return avg;
 }
